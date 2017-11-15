ee6a5be68d344b14e2620f42007caa548ffa0339
Cadulu <cadulu@github.com>
2017-11-15 16:24:13 -0200

    Conclusão da Aula #3.1(Teórica)
    Strings e Funções Especiais.
    Próxima Aula: #3.2(Prática)

--- strings.php
+++ strings.php
@@ -3,12 +3,19 @@
 <head>
 	<title>SoftBlue::Strings</title>
 	<style>
-		@import url('https://fonts.googleapis.com/css?family=Indie+Flower');
-		body { font-family: 'Indie Flower', cursive; }
+		
+		@import url('https://fonts.googleapis.com/css?family=Josefin+Sans');
+		body { 
+			
+			font-family: 'Josefin Sans', sans-serif;
+			color: #fff;
+			background: #808080; }
+
 	</style>
 </head>
 <body>
-	<?php  
+	<?php
+		define("BR", "<br>");
 		$strExemplo = "PHP é na SoftBlue";
 		echo ($strExemplo);
 		$tamanho = strlen($strExemplo);
@@ -30,6 +37,51 @@
 		echo "<br>"."A substring de:".'"'.$strExemplo.'"'." a partir da posição 4 é: ".$x;
 		$x = substr($strExemplo, 4,6);
 		echo "<br>"."A substring de:".'"'.$strExemplo.'"'." com 6 caracteres a partir da 4a posição é: ".'"'.$x.'"';
+		$x = str_replace("na", "com a", $strExemplo);
+		echo "<br>".'Substindo "na" por "com a" em '.'"'.$strExemplo.'", fica: '.$x.'.'.BR;
+		$imagem = "worldofwarcraft.jpg";
+		echo '<br><img src='.$imagem.'>';
+		echo "<br> Substituição da Imagem por MOP:";
+		echo "<br>".'<img src='.str_replace("worldofwarcraft.jpg","mistsofpandaria.png",$imagem).">";
+		$inversao = "Eduardo Luciano";
+		echo "<br>"."O valor da variável é: ".$inversao;
+		echo "<br> O Valor inverso é: ".strrev($inversao);
+		echo "<br> Versão atual do PHP: ".phpversion();
+		echo BR."Criptografia de string:".BR."crypt:".BR;
+		$strExemplo = "PHP é na SoftBlue";
+		$x = crypt($strExemplo);
+		echo "O valor original é: ".$strExemplo.BR."O valor criptografado (crypt) é: ".$x.BR;
+		$x = sha1($strExemplo);
+		echo "O valor original é: ".$strExemplo.BR."O valor criptografado (sha1) é: ".$x.BR;
+		$x = md5($strExemplo);
+		echo "O valor original é: ".$strExemplo.BR."O valor criptografado (md5): ".$x.BR;
+		echo "Usando o str_split com 4 posições:".BR;
+		$x = str_split($strExemplo,4);
+		$i=0;
+		for ($i=0; $i < 4; $i++) { 
+			echo $x[$i].BR;
+		}
+		$x = trim($strExemplo);
+		$x = str_replace(" "," :-) ",$x);
+		echo $x.BR;
+		$x = ltrim($strExemplo);
+		$x = str_replace(" "," :-) ",$x);
+		echo $x.BR;
+		$x = rtrim($strExemplo);
+		$x = str_replace(" "," :-) ",$x);
+		echo $x.BR;		
+		echo "Casting de valores:".BR;
+		$strExemplo = "15";
+		print 'Valor da  variável: '.$strExemplo.BR;
+		echo'Valor da ++variável: '.(++$strExemplo).BR;
+		$strExemplo= "a15b40";
+		print "Valor da  variável: ".$strExemplo.BR;
+		print "Valor da ++variável: ".++$strExemplo.BR;
+		$strExemplo="a15b40c";
+		print "Valor da  variável: ".$strExemplo.BR;
+		print "Valor da ++variável: ".++$strExemplo.BR;
+		
+
 	?>
 
 </body>
